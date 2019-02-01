77894744d4bc9f2b55822b7ac4694039821e9eed
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2019-02-01 15:14:55 -0500

    Move stuff around

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Toggle from '../Toggle/Toggle';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Toggle />
+      </div>
+    );
+  }
+}
+
+export default App;
